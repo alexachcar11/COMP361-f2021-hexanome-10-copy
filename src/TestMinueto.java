575fdc6ef191876e6b46ce9bd0b0b09e8a96bca7
liliahache <lilia@concubine.ca>
2021-10-19 15:18:39 -0400

    add minueto

---
+++ src/TestMinueto.java
@@ -0,0 +1,31 @@
+import org.minueto.*;
+import org.minueto.handlers.*;
+import org.minueto.image.*;
+import org.minueto.window.*;
+
+
+public class TestMinueto {
+
+    public static void main(String[] args) {
+        MinuetoWindow window;
+        MinuetoImage demoImage;
+
+        window = new MinuetoFrame(640, 480, true);
+
+        try {
+            demoImage = new MinuetoImageFile("böppels-and-boots/boot-black.png");
+        } catch(MinuetoFileException e) {
+            System.out.println("Could not load image file");
+            return;
+        }
+
+        window.setVisible(true);
+
+        while(true) {
+            window.draw(demoImage, 0, 0);
+            window.render();
+            Thread.yield();
+        }
+    }
+
+}
