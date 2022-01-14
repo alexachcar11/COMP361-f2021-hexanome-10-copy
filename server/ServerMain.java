c10a5b3fcdf0598a4e8a3b15be6d395efb8282e0
liliahache <lilia@concubine.ca>
2022-01-13 20:00:39 -0500

    remove useless import statements in ServerMain

--- server/ServerMain.java
+++ server/ServerMain.java
@@ -1,16 +1,17 @@
+// API requests and parsing
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+// other
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Set;
 
 public class ServerMain {
     /*
