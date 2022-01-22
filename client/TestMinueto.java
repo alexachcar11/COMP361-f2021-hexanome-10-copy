818db990591e796d15ab615146b54f2065a48361
liliahache <lilia@concubine.ca>
2022-01-22 00:26:02 -0500

    do some testing with Swing and Minueto

--- client/TestMinueto.java
+++ client/TestMinueto.java
@@ -1,33 +1,37 @@
 import org.minueto.*;
-import org.minueto.handlers.*;
 import org.minueto.image.*;
 import org.minueto.window.*;
+import java.io.IOException;
 
 public class TestMinueto {
 
-    public static void main(String[] args) {
-        MinuetoWindow window;
-        MinuetoImage demoImage;
+        public static void main(String[] args) throws IOException {
+            // MINUETO TESTING
+            MinuetoWindow window;
+            MinuetoImage demoImage;
 
-        MinuetoRectangle rectangle = new MinuetoRectangle(50, 1000, MinuetoColor.WHITE, true);
+            MinuetoRectangle rectangle = new MinuetoRectangle(1000, 1000, MinuetoColor.BLUE, true);
 
-        window = new MinuetoFrame(640, 480, true);
-        window.setMaxFrameRate(60);
+            window = new MinuetoFrame(1024, 768, true);
+            window.setMaxFrameRate(60);
 
-        try {
-            demoImage = new MinuetoImageFile("böppels-and-boots/boot-black.png");
-        } catch (MinuetoFileException e) {
-            System.out.println("Could not load image file");
-            return;
+            try {
+                demoImage = new MinuetoImageFile("images/böppels-and-boots/boot-black.png");
+            } catch (MinuetoFileException e) {
+                System.out.println("Could not load image file");
+                return;
+            }
+
+            window.setVisible(true);
+
+            while (true) {
+                window.draw(rectangle, 10, 10);
+                window.draw(demoImage, 20,20);
+
+
+                window.render();
+                Thread.yield();
+            }
+            // MINUETO TESTING END
         }
-
-        window.setVisible(true);
-
-        while (true) {
-            window.draw(rectangle, 10, 10);
-            window.render();
-            Thread.yield();
-        }
-    }
-
 }
