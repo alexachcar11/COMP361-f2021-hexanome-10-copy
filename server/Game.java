d31f4b79c9e3cc7021176e51365f4f8223093699
liliahache <lilia@concubine.ca>
2022-02-10 15:36:24 -0500

    started Game class on the server

---
+++ server/Game.java
@@ -0,0 +1,22 @@
+/**
+ * Represents one playable game.
+ */
+
+public class Game {
+
+    // FIELDS
+    int currentPhase;
+    int currenRound;
+    int gameRoundsLimit;
+    boolean destinationTownEnabled;
+    boolean witchEnabled;
+    Mode mode;
+
+
+    // CONSTRUCTOR
+
+
+    // METHODS
+
+
+}
