14bc363c099af4da2b9e684c055812efef84fd63
liliahache <lilia@concubine.ca>
2022-02-10 16:37:56 -0500

    started Game class on the server

--- server/Game.java
+++ server/Game.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Represents one playable game.
  */
@@ -6,17 +8,26 @@ public class Game {
 
     // FIELDS
     int currentPhase;
-    int currenRound;
+    int currentRound;
     int gameRoundsLimit;
     boolean destinationTownEnabled;
     boolean witchEnabled;
     Mode mode;
+    ArrayList<Player> players;
 
 
     // CONSTRUCTOR
+    public Game(int gameRoundsLimit, boolean destinationTownEnabled, boolean witchEnabled, Mode mode) {
+        this.gameRoundsLimit = gameRoundsLimit;
+        this.destinationTownEnabled = destinationTownEnabled;
+        this.witchEnabled = witchEnabled;
+        this.mode = mode;
+        this.currentPhase = 1;
+        this.currentRound = 1;
+        this.players = new ArrayList<>();
+    }
 
 
     // METHODS
-
 
 }
