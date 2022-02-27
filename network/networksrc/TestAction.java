8e84de9cc73c6570974fdc7ef19b206ace90ff86
liliahache <lilia@concubine.ca>
2022-02-27 16:26:20 -0500

    testActionConfirmation test

--- network/networksrc/TestAction.java
+++ network/networksrc/TestAction.java
@@ -1,5 +1,12 @@
 package networksrc;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class TestAction extends Action {
+
+    private Socket socket;
 
     private String test = "The network is working!!!";
 
@@ -10,7 +17,30 @@ public class TestAction extends Action {
 
     @Override
     public void execute() {
+        // server has received the message
         System.out.println(test);
+        // client receives a confirmation
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject(new TestActionConfirmation());
+        } catch (IOException e) {
+            System.err.println("IOException in TestAction.execute().");
+        }
+        
+    }
+
+    public class TestActionConfirmation extends Action{
+
+        @Override
+        public boolean isValid() {
+            return false;
+        }
+
+        @Override
+        public void execute() {
+            System.out.println("TestActionConfirmation is working!");
+        }
+
     }
 
 }
