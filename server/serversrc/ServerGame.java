c7a9b4da2469d2d0a32a7dd29f14022f9f0f5611
Potato455 <90859863+Potato455@users.noreply.github.com>
2022-02-27 19:26:17 -0500

    fixed an error with Token by adding a transportationCounter class

--- server/serversrc/ServerGame.java
+++ server/serversrc/ServerGame.java
@@ -175,7 +175,7 @@ public class ServerGame {
             //create tokens and add to list
             for (int j = 0; j<6; j++){
                 for (int i = 0; i < 8; i++){
-                    Token tok = new Token(CardType.values()[j]);
+                    Token tok = new TransportationCounter(CardType.values()[j]);
                     allTokens.add(tok);
                 }
             }
