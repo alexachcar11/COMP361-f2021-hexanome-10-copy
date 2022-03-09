8d004a4ef00bbb485cb38b64b26258b52c520834
Owen Glahn <owen.glahn@mail.mcgill.ca>
2022-03-08 23:12:54 -0500

    merge lobbyservice

--- network/networksrc/LaunchGameACK.java
+++ network/networksrc/LaunchGameACK.java
@@ -4,18 +4,17 @@ import clientsrc.ClientMain;
 
 import java.util.ArrayList;
 
-public class LaunchGameACK implements Action{
+public class LaunchGameACK implements Action {
 
     private ArrayList<String> playerNames;
     private ArrayList<ArrayList<String>> playerCards;
 
     // TODO: store the game state through a constructor
-    public LaunchGameACK(ArrayList<String> playerNames, ArrayList<ArrayList<String>> playerCards){
+    public LaunchGameACK(ArrayList<String> playerNames, ArrayList<ArrayList<String>> playerCards) {
 
         this.playerNames = playerNames;
         this.playerCards = playerCards;
     }
-
 
     @Override
     public boolean isValid() {
@@ -28,18 +27,8 @@ public class LaunchGameACK implements Action{
         // set the session to launched
         ClientMain.currentSession.launch();
 
-<<<<<<< HEAD
-        // modify game objects based on the game state received
-        // TODO: iterate thru playerNames, index for loop --> use to iterate thru playerCards, give Players their cards :)
-
-
-        // display the new board
-
-        System.out.println("LaunchGameACK received");
-=======
         // display board screen
         ClientMain.displayOriginalBoard();
->>>>>>> lobbyservice
     }
-    
+
 }
