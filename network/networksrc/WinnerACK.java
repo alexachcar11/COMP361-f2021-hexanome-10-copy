093200428236f4146335ef0b1ecf03074c45f34d
Owen Glahn <owen.glahn@mail.mcgill.ca>
2022-03-09 23:48:47 -0500

    fix infinite error loop

--- network/networksrc/WinnerACK.java
+++ network/networksrc/WinnerACK.java
@@ -1,18 +1,15 @@
 package networksrc;
 
-<<<<<<< Updated upstream
-=======
 import org.minueto.MinuetoColor;
 import org.minueto.MinuetoFileException;
 
->>>>>>> Stashed changes
 import clientsrc.ClientMain;
 
-public class WinnerACK implements Action{
+public class WinnerACK implements Action {
 
     private String winnerName;
 
-    public WinnerACK(String name){
+    public WinnerACK(String name) {
         this.winnerName = name;
     }
 
@@ -24,12 +21,12 @@ public class WinnerACK implements Action{
     @Override
     public void execute() {
         // display winner
-        try {
-            ClientMain.displayWinnerByString(winnerName);
-        } catch (MinuetoFileException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        // try {
+        // ClientMain.displayWinnerByString(winnerName);
+        // } catch (MinuetoFileException e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
     }
-    
+
 }
