3c474f1faf71804ba689d4ad628158488a77a744
elfen <lilia@concubine.ca>
2022-03-10 05:10:14 +0000

    merge issue

--- network/networksrc/WinnerACK.java
+++ network/networksrc/WinnerACK.java
@@ -1,11 +1,5 @@
 package networksrc;
 
-<<<<<<< Updated upstream
-=======
-import org.minueto.MinuetoColor;
-import org.minueto.MinuetoFileException;
-
->>>>>>> Stashed changes
 import clientsrc.ClientMain;
 
 public class WinnerACK implements Action{
@@ -24,12 +18,12 @@ public class WinnerACK implements Action{
     @Override
     public void execute() {
         // display winner
-        try {
+        /* try {
             ClientMain.displayWinnerByString(winnerName);
         } catch (MinuetoFileException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }
+        } */
     }
     
 }
