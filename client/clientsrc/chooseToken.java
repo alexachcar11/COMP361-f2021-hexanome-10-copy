8b13f122ccc7c0b6bb4b491b9dffd2544d722a8a
alexander-achcar <alexander.achcar@mail.mcgill.ca>
2022-03-19 06:13:19 -0400

    Update chooseToken.java
    Added implementation to click on a button and get a token added to your inventory

--- client/clientsrc/chooseToken.java
+++ client/clientsrc/chooseToken.java
@@ -3,17 +3,20 @@ package clientsrc;
 import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputListener;
 
 import org.minueto.MinuetoFileException;
 import org.minueto.image.MinuetoImage;
 import org.minueto.image.MinuetoImageFile;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.*;
 
 public class chooseToken {
     
     private ArrayList<TokenImage> listOfAvailableTokens = new ArrayList<>(); 
-    private Player player; 
+    private Player player;     
 
     public chooseToken(Player p, ArrayList<TokenImage> tokensToChooseFrom) throws MinuetoFileException { 
 
@@ -24,6 +27,7 @@ public class chooseToken {
     }
 
     public void openInterface() throws MinuetoFileException { 
+
         JPanel chooseTokenPanel = new JPanel(); 
         chooseTokenPanel.setLayout(new BoxLayout(chooseTokenPanel, BoxLayout.Y_AXIS));
 
@@ -41,14 +45,48 @@ public class chooseToken {
         headerTitle.add(pickTokenText);
 
         // ADD IMAGES FOR THE DIFFERENT TOKEN OPTIONS 
-        for(TokenImage tok : listOfAvailableTokens) { 
 
-            // obtain the address of the token 
-            String address = tok.getFileAdress();
-            JLabel picOfToken = new JLabel(new ImageIcon(address));
-            tokenOptions.add(picOfToken);
+        // first token 
+        String address1 = listOfAvailableTokens.get(0).getFileAdress();
+        JLabel picOfToken1 = new JLabel(new ImageIcon(address1));
+        JButton picButton1 = new JButton();
+        picButton1.add(picOfToken1);
 
-        }
+        // second token 
+        String address2 = listOfAvailableTokens.get(0).getFileAdress();
+        JLabel picOfToken2 = new JLabel(new ImageIcon(address2));
+        JButton picButton2 = new JButton();
+        picButton2.add(picOfToken2);
+    
+        // second token 
+        String address3 = listOfAvailableTokens.get(0).getFileAdress();
+        JLabel picOfToken3 = new JLabel(new ImageIcon(address3));
+        JButton picButton3 = new JButton();
+        picButton2.add(picOfToken3);
+
+        // second token 
+        String address4 = listOfAvailableTokens.get(0).getFileAdress();
+        JLabel picOfToken4 = new JLabel(new ImageIcon(address4));
+        JButton picButton4 = new JButton();
+        picButton4.add(picOfToken4);
+
+        // second token 
+        String address5 = listOfAvailableTokens.get(0).getFileAdress();
+        JLabel picOfToken5 = new JLabel(new ImageIcon(address5));
+        JButton picButton5 = new JButton();
+        picButton5.add(picOfToken5);
+
+
+        ///////  add buttons to tokenOptions
+
+        tokenOptions.add(picButton1);
+        tokenOptions.add(picButton2);
+        tokenOptions.add(picButton3);
+        tokenOptions.add(picButton4);
+        tokenOptions.add(picButton5);
+
+
+        ////////
 
         chooseTokenPanel.add(Box.createVerticalStrut(30));
         chooseTokenPanel.add(headerTitle);
@@ -61,6 +99,94 @@ public class chooseToken {
         tokenPanelOverview.setSize(new Dimension(700, 300));
 
         tokenPanelOverview.setVisible(true);
+
+        ///////// clicking on a token
+
+        picButton1.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent buttonClicked) {
+                                
+                // If the user clicks on the token, add the token the inventory of the player and close the window
+                try {
+                    player.addTokenString(listOfAvailableTokens.get(0).getName());
+                    tokenPanelOverview.dispose();
+                } catch (MinuetoFileException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        picButton2.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent buttonClicked) {
+                                
+                // If the user clicks on the token, add the token the inventory of the player and close the window
+                try {
+                    player.addTokenString(listOfAvailableTokens.get(1).getName());
+                    tokenPanelOverview.dispose();
+                } catch (MinuetoFileException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        picButton3.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent buttonClicked) {
+                                
+                // If the user clicks on the token, add the token the inventory of the player and close the window
+                try {
+                    player.addTokenString(listOfAvailableTokens.get(2).getName());
+                    tokenPanelOverview.dispose();
+                } catch (MinuetoFileException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        picButton4.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent buttonClicked) {
+                                
+                // If the user clicks on the token, add the token the inventory of the player and close the window
+                try {
+                    player.addTokenString(listOfAvailableTokens.get(3).getName());
+                    tokenPanelOverview.dispose();
+                } catch (MinuetoFileException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        picButton5.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent buttonClicked) {
+                                
+                // If the user clicks on the token, add the token the inventory of the player and close the window
+                try {
+                    player.addTokenString(listOfAvailableTokens.get(4).getName());
+                    tokenPanelOverview.dispose();
+                } catch (MinuetoFileException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        
     }
 
 }
