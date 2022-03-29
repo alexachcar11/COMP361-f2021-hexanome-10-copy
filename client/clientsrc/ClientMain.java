773d62f1d70bc13df63760bc86874bb9eb764c1b
liliahache <lilia@concubine.ca>
2022-03-28 21:37:12 -0400

    fix displayGames

--- client/clientsrc/ClientMain.java
+++ client/clientsrc/ClientMain.java
@@ -1440,6 +1440,7 @@ public class ClientMain {
                 }
 
             } else if (gui.currentBackground == GUI.Screen.LOBBY) {
+                gui.window.draw(lobbyBackground, 0, 0);
                 while (lobbyScreenQueue.hasNext()) {
                     lobbyScreenQueue.handle();
                 }
@@ -2416,8 +2417,8 @@ public class ClientMain {
         joinButtonCoordinates.clear();
 
         // display
-        gui.window.draw(lobbyBackground, 0, 0);
         gui.currentBackground = GUI.Screen.LOBBY;
+        gui.window.draw(lobbyBackground, 0, 0);
         MinuetoFont font = new MinuetoFont("Arial", 22, true, false);
         try {
             ArrayList<LobbyServiceGameSession> availableSessionsList = LobbyServiceGameSession.getAvailableSessions();
