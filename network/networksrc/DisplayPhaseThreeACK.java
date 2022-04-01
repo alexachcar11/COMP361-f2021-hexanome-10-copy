89b247888192a4ace7558eb93743ff3870d0b81e
Owen Glahn <owen.glahn@mail.mcgill.ca>
2022-04-01 00:24:03 -0400

    add swing to DisplayPhaseThreeACK, make SwingTokenSprite to allow easier compatibility with Swing

--- network/networksrc/DisplayPhaseThreeACK.java
+++ network/networksrc/DisplayPhaseThreeACK.java
@@ -1,20 +1,20 @@
 package networksrc;
 
+import java.awt.event.MouseEvent;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
 
-import org.minueto.MinuetoEventQueue;
 import org.minueto.MinuetoFileException;
-import org.minueto.handlers.MinuetoMouseHandler;
 import org.minueto.window.MinuetoWindow;
 
 import clientsrc.ClientMain;
+import clientsrc.SwingTokenSprite;
 import clientsrc.TokenSprite;
 
 public class DisplayPhaseThreeACK implements Action {
@@ -44,42 +44,56 @@ public class DisplayPhaseThreeACK implements Action {
             return null;
         })
                 .collect(Collectors.toList());
-        int count = 0;
-        JFrame tokenFrame = new JFrame("Select a token.");
-        JPanel tokenPanel = new JPanel();
-        tokenPanel.setLayout(new BoxLayout(tokenPanel, BoxLayout.X_AXIS));
-        tokenImages.forEach((tokenImage) -> {
-            JLabel pic = new JLabel(new ImageIcon(tokenImage.getFileAddress()));
-            tokenPanel.add(pic);
-        });
-        tokenFrame.add(tokenPanel);
-        tokenFrame.setVisible(true);
-        // mouse handler for selecting token from face up tokens
-        MinuetoMouseHandler tokenSelect = new MinuetoMouseHandler() {
+        MouseInputListener mouseListener = new MouseInputListener() {
 
             @Override
-            public void handleMousePress(int xClicked, int yClicked, int arg2) {
-                for (TokenSprite t : tokenImages) {
-                    if (t.hasCollidePoint(xClicked, yClicked)) {
-                        // inform server that user has selected t
-                        ActionManager.getInstance()
-                                .sendAction(new TokenSelectedAction(
-                                        ClientMain.currentSession.getSessionID(), t.getTokenName()));
-                        break;
-                    }
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                try {
+                    JLabel origin = (JLabel) e.getComponent();
+                    SwingTokenSprite sprite = (SwingTokenSprite) origin.getIcon();
+                    String gameID = ClientMain.currentSession.getSessionID();
+                    ActionManager.getInstance()
+                            .sendAction(new TokenSelectedAction(gameID, sprite.getTypeString()));
+                } catch (ClassCastException exception) {
+                    // do nothing if not a JLabel
+                    return;
                 }
             }
 
             @Override
-            public void handleMouseRelease(int arg0, int arg1, int arg2) {
+            public void mouseReleased(MouseEvent e) {
             }
 
             @Override
-            public void handleMouseMove(int arg0, int arg1) {
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
             }
         };
-        MinuetoEventQueue selectTokenQueue = new MinuetoEventQueue();
-        window.registerMouseHandler(tokenSelect, selectTokenQueue);
+        JFrame tokenFrame = new JFrame("Select a token.");
+        JPanel tokenPanel = new JPanel();
+        tokenPanel.setLayout(new BoxLayout(tokenPanel, BoxLayout.X_AXIS));
+        tokenImages.forEach((tokenImage) -> {
+            JLabel pic = new JLabel(new SwingTokenSprite(tokenImage));
+            pic.addMouseListener(mouseListener);
+            tokenPanel.add(pic);
+        });
+        tokenFrame.add(tokenPanel);
+        tokenFrame.setVisible(true);
         window.render();
         Thread.yield();
     }
